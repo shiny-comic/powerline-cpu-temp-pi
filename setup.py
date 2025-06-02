cca0f054172211af01c806ef41379fbd4c3c5033
shiny-comic <shiny-comic@winPC>
2025-06-03 00:21:57 +0900

    style: format

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
 from setuptools import setup
 from os import path
 
-version = '0.1.0'
+version = "0.1.0"
 
 repo_base_dir = path.abspath(path.dirname(__file__))
 
 # Long description
-readme = path.join(repo_base_dir, 'README.md')
+readme = path.join(repo_base_dir, "README.md")
 with open(readme) as f:
     long_description = f.read()
 
 setup(
     name="powerline-cpu-temp-pi",
     version=version,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     description="An improved Powerline segment to show CPU temperature",
     long_description=long_description,
-    author_email='DCsunset@protonmail.com',
-    license='MIT',
-
-    install_requires=['psutil'],
+    author_email="DCsunset@protonmail.com",
+    license="MIT",
+    install_requires=["psutil"],
     author="shiny-comic",
     url="https://github.com/shiny-comic/powerline-cpu-temp-pi",
     # Add to lib so that it can be included
-    py_modules=['powerline_cpu_temp'],
-
+    py_modules=["powerline_cpu_temp"],
     classifiers=[
-        'Environment :: Console',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Terminals'
-    ]
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Terminals",
+    ],
 )
